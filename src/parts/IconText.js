ce2222d5d0133b51d1144876e56480ba76e394a0
johnvees <yohanesvelly23@gmail.com>
2022-05-19 13:23:30 +0700

    update global variable color

---
+++ src/parts/IconText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+
+export default function IconText() {
+  return (
+    <Button className='brand-text-icon' href='' type='link'> 
+        Test<span className='text-yellow'>123</span>
+    </Button>
+  )
+}
